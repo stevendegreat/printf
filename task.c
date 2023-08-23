84adce428f605edd07f5267e53520dede49a5b63
stevendegreat <stevendenjuguna@gmail.com>
2023-08-23 05:07:27 -0700

    printf

--- task.c
+++ task.c
@@ -9,67 +9,67 @@
 int _printf(const char *format, ...)
 {
 
-	int char_print = 0;
+int char_print = 0;
 
-	va_list arg_list;
+va_list arg_list;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	va_start(arg_list, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			char_print++;
-		}
-		else
-		{
-			format++;
-			if (*format  == '\0')
-				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				char_print++;
-			}
+if (format == NULL)
+{
+return (-1);
+}
+va_start(arg_list, format);
+while (*format)
+{
+if (*format != '%')
+{
+write(1, format, 1);
+char_print++;
+}
+else
+{
+format++;
+if (*format  == '\0')
+break;
+if (*format == '%')
+{
+write(1, format, 1);
+char_print++;
+}
 
-			else if (*format == 'c')
-			{
-				char c = va_arg(arg_list, int);
+else if (*format == 'c')
+{
+char c = va_arg(arg_list, int);
 
-				write(1, &c, 1);
-				char_print++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(arg_list, char*);
+write(1, &c, 1);
+char_print++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(arg_list, char*);
 
-				if (str == NULL)
-				{
-					write(1, "(null)", 6);
-					char_print += 6;
-				}
-				else
-				{
-					while (*str)
-					{
-						write(1, str, 1);
-						char_print++;
-						str++;
-					}
-				}
-			}
-			else
-			{
-				write(1, format - 1, 2);
-				char_print += 2;
-			}
-		}
-		format++;
-	}
-	va_end(arg_list);
-	return (char_print);
+if (str == NULL)
+{
+write(1, "(null)", 6);
+char_print += 6;
+}
+else
+{
+while (*str)
+{
+write(1, str, 1);
+char_print++;
+str++;
+}
+}
+}
+else
+{
+write(1, format - 1, 2);
+char_print += 2;
+}
+}
+format++;
+}
+va_end(arg_list);
+return (char_print);
 }
