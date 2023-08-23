9e86a1204dd52e2d5d4e61b70fe27e2ab06f5712
stevendegreat <stevendenjuguna@gmail.com>
2023-08-23 04:58:06 -0700

    printf

--- task1.c
+++ task1.c
@@ -1,48 +1,45 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
 
-/**
- * _printf - Custom printf function
- * @format: Format string
- *
- * Return: Number of characters printed
- **/
 int _printf(const char *format, ...)
 {
-	int char_print = 0;
-	va_list arg_list;
+    int char_print = 0;
+    va_list arg_list;
 
-	va_start(arg_list, format);
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			char_print++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(arg_list, int);
-				char buffer[20]; /* Assuming a maximum of 20 characters for the number */
+    va_start(arg_list, format);
 
-				int num_chars = snprintf(buffer, sizeof(buffer), "%d", num);
-				write(1, buffer, num_chars);
-				char_print += num_chars;
-			}
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            char_print++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(arg_list, int);
+                char buffer[20]; /*Assuming a maximum of 20 characters for the number*/
 
-	va_end(arg_list);
+                int num_chars = snprintf(buffer, sizeof(buffer), "%d", num);
+                write(1, buffer, num_chars);
+                char_print += num_chars;
+            }
+            /*Add handling for other specifiers here*/
+            else
+            {
+                /*Handle unknown specifier*/
+                write(1, "Unsupported format specifier", sizeof("Unsupported format specifier") - 1);
+                char_print += sizeof("Unsupported format specifier") - 1;
+            }
+        }
+        format++;
+    }
 
+    va_end(arg_list);
 
-	format++;
-
-	return (char_print);
+    return (char_print);
 }
